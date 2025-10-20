48729ace1041208fe10b5fc49a17b3b0ce50f0f6
Joel Müller <joelmueller27@gmail.com>
2025-10-20 15:01:56 +0200

    nvim - keep mouse the same shape in all modes

--- nvim/init.lua
+++ nvim/init.lua
@@ -106,6 +106,7 @@ vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = ''
+vim.o.guicursor = ''
 
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
