18bdbd16064e3583a9eb2c20b49dd566fd23ca41
Joel Müller <joelmueller27@gmail.com>
2026-06-07 16:28:16 +0200

    nvim - remove auto format on write

--- dotfiles/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -1,38 +1,31 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{ "williamboman/mason.nvim", config = true },
-			"williamboman/mason-lspconfig.nvim",
-		},
-		config = function()
-			require("mason-lspconfig").setup({ ensure_installed = { "gopls", "lua_ls" } })
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{ "williamboman/mason.nvim", config = true },
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		require("mason-lspconfig").setup({ ensure_installed = { "gopls", "lua_ls" } })
 
-			vim.lsp.config("lua_ls", {
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
 					},
 				},
-			})
+			},
+		})
 
-			vim.lsp.enable({ "gopls", "lua_ls" })
+		vim.lsp.enable({ "gopls", "lua_ls" })
 
-			vim.api.nvim_create_autocmd("LspAttach", {
-				callback = function(ev)
-					vim.keymap.set("n", "grn", vim.lsp.buf.rename, { buffer = ev.buf })
-					vim.keymap.set("n", "grd", vim.lsp.buf.definition, { buffer = ev.buf })
-					vim.keymap.set("n", "grr", vim.lsp.buf.references, { buffer = ev.buf })
-					vim.keymap.set("n", "ge", vim.diagnostic.open_float, { buffer = ev.buf })
-				end,
-			})
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				callback = function()
-					vim.lsp.buf.format()
-				end,
-			})
-		end,
-	},
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(ev)
+				vim.keymap.set("n", "grn", vim.lsp.buf.rename, { buffer = ev.buf })
+				vim.keymap.set("n", "grd", vim.lsp.buf.definition, { buffer = ev.buf })
+				vim.keymap.set("n", "grr", vim.lsp.buf.references, { buffer = ev.buf })
+				vim.keymap.set("n", "ge", vim.diagnostic.open_float, { buffer = ev.buf })
+			end,
+		})
+	end,
 }
