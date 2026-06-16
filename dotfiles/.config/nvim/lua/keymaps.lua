be8e6d1560b2c5d4d95ac2e77234f852513669c0
Joel Müller <joelmueller27@gmail.com>
2026-06-16 21:22:51 +0200

    nvim - make L and H work for operator

--- dotfiles/.config/nvim/lua/keymaps.lua
+++ dotfiles/.config/nvim/lua/keymaps.lua
@@ -2,6 +2,6 @@ local map = vim.keymap.set
 
 map({ "n", "v" }, "<C-d>", "<C-d>zz")
 map({ "n", "v" }, "<C-u>", "<C-u>zz")
-map({ "n", "v" }, "H", "^")
-map({ "n", "v" }, "L", "$")
+map({ "n", "v", "o" }, "L", "$")
+map({ "n", "v", "o" }, "H", "^")
 map("i", "<C-c>", "<Esc>")
